e944e2de8444c5e75e6f1b8eee0edc997132fc8b
giusmili <giusmili67@gmail.com>
2023-11-20 16:35:44 +0100

    code

--- js/app_init.js
+++ js/app_init.js
@@ -1,4 +1,13 @@
 
+export const City = {
+    image: "./asset/cover.jpg",
+    description: `New York est une ville composée de 5 arrondissements à l'embouchure du fleuve Hudson et de l'océan Atlantique. En son centre se trouve Manhattan, un arrondissement densément peuplé faisant partie des principaux centres commerciaux, financiers et culturels du monde. Ses sites incontournables comprennent des gratte-ciel comme l'Empire State Building et l'immense Central Park. Le théâtre de Broadway est situé sur Times Square`,
+    Météo: `6°C, vent O à 6 km/h, 68 % d'humidité weather.com`,
+    Quartier: `Harlem, Upper East Side, West Village, Flushing, SoHo, PLUS`,
+    Population: `8,468 millions (2021)`,
+    Altitude: `10 m; Min. −8,2 m; Max. 124,9 m`,
+    Coordonnées: `40° 42′ 51″ nord, 74° 00′ 21″ ouest `
+};
 
 export const Tamplate = {
     el: document.querySelector("section"),
