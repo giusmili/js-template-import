414b02661624e377e176649be1a514f7e4cb22dd
giusmili <giusmili67@gmail.com>
2023-11-20 16:31:52 +0100

    js template

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 import { Description } from "./app_init.js";
 import { Tamplate } from "./app_init.js";
 import { msg } from "./app_init.js";
+import { City } from "./template.js";
 
 document.addEventListener("DOMContentLoaded", e => {
+
     console.log(e.target);
     console.table(Description);
     Tamplate.printF();
     console.log(msg);
+
+    const json = JSON.stringify(City);
+    console.log(json + "\n");
+
+    for (let index in City) {
+        console.log(index + " " + City[index] + '\n');
+    }
 });
